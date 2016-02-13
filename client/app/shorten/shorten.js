5494b8129203b8d0d4724845a1b8bb0fb381548d
elliotaplant <elliotaplant@gmail.com>
2016-02-12 18:59:04 -0800

    Clean up shorten

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,8 +2,7 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.addLink = function () {
-    console.log('The thing you just input: ', $scope.inputLink);
-    Links.addOne( { url: $scope.inputLink } ); // note that the url property of $location is a function. This may bite us later
+    Links.addOne( { url: $scope.inputLink } );
     $scope.inputLink = '';
   };
   $scope.link = {};
