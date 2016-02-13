91256acdac569f3a8641d16ad0ebea6b53cdeb7f
elliotaplant <elliotaplant@gmail.com>
2016-02-12 17:01:25 -0800

    Clear input field upon submission

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,6 +3,7 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.addLink = function () {
     Links.addOne($location); // note that the url property of $location is a function. This may bite us later
+    $scope.inputLink = '';
   };
   $scope.link = {};
 });
