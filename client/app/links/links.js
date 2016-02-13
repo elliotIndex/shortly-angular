e87613ae973a7cae87fdcff19545b23ec75df8ec
elliotaplant <elliotaplant@gmail.com>
2016-02-12 19:37:32 -0800

    Add signout button, redirect to signin

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,6 +1,7 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
+  $scope.links = Links.getAll();
   $scope.data = {};
   Links.getAll()
     .then(function (links) {
