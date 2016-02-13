6aea536cea92954aa3da9665f4027d25b703ca84
elliotaplant <elliotaplant@gmail.com>
2016-02-12 16:16:13 -0800

    Create new links with addLink

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,6 +1,8 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  $scope.addLink = function () {};
+  $scope.addLink = function () {
+    Links.addOne($location); // note that the url property of $location is a function. This may bite us later
+  };
   $scope.link = {};
 });
