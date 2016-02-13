b4760ecaeb8da8cdfad92ee439076292874b45ac
Elliot Plant <elliotaplant@gmail.com>
2016-02-13 11:04:50 -0800

    Alert user for invalid username and/or password
    For signin and signup pages

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -13,7 +13,7 @@ angular.module('shortly.auth', [])
         $location.path('/links');
       })
       .catch(function (error) {
-        console.error(error);
+        alert('Invalid username and/or password');
       });
   };
 
@@ -24,7 +24,7 @@ angular.module('shortly.auth', [])
         $location.path('/links');
       })
       .catch(function (error) {
-        console.error(error);
+        alert('Invalid username and/or password');
       });
   };
 
